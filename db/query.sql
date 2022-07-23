3a7dc6579d6191001c5b7392459205e537e2c0d5
Kaysie <kaysielandersonwork@gmail.com>
2022-07-23 10:34:48 -0500

    created database

---
+++ db/query.sql
@@ -0,0 +1,13 @@
+SELECT id, department_name AS department 
+FROM department;
+
+SELECT role.id, title, salary, department_name AS department
+FROM role
+INNER JOIN department ON role.department_id = department.id;
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.department_name AS department, employee.manager_id
+FROM employee
+INNER JOIN role ON employee.role_id = role.id
+INNER JOIN department ON role.department_id = department.id;
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO department
+  (department_name)
+VALUES
+('Administration'),
+('Marketing'),
+('Sales'),
+('Shipping'),
+('Retail');
+
+INSERT INTO role 
+(title, salary, department_id)
+VALUES 
+('HR Rep', '500000.00', 1),
+('Social Media Rep', '400000.00', 2),
+('Sales Rep', '300000.00', 3),
+('Shipping Manager', '200000.00', 4),
+('Cashier', '100000.00', 5);
+
+INSERT INTO employee
+(first_name, last_name, role_id, manager_id)
+VALUES
+('Kaysie', 'Anderson', 1, 1),
+('Temple', 'Kramer', 2, NULL),
+('Caitlin', 'Parsons', 3, NULL),
+('Josie', 'Sparling', 4, NULL),
+('Todd', 'Holley', 5, NULL);
+
